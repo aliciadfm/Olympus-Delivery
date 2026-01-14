ce714f859d60ed250637788b37c5b8b97766a52b
Pau Martínez Lledó <paumarlle@hotmail.com>
2026-01-14 15:40:14 +0100

    Reformular diálogos y martillo arreglado

--- Assets/Scripts/Player/Martillo.cs
+++ Assets/Scripts/Player/Martillo.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 public class HammerTrigger : MonoBehaviour
 {
     [SerializeField] private GameObject hammer;
+    [SerializeField] private GameObject hammer2;
     [SerializeField] private GameObject characterBefore;
     [SerializeField] private GameObject characterAfter;
+    [SerializeField] private GameObject mobile;
     [SerializeField] private string playerTag = "Player";
 
     private void OnTriggerEnter(Collider other)
@@ -12,7 +14,9 @@ public class HammerTrigger : MonoBehaviour
         if (!other.CompareTag(playerTag)) return;
 
         if (hammer) hammer.SetActive(false);
+	if (hammer) hammer2.SetActive(true);
         if (characterBefore) characterBefore.SetActive(false);
         if (characterAfter) characterAfter.SetActive(true);
+	if (mobile) characterAfter.SetActive(true);
     }
 }
